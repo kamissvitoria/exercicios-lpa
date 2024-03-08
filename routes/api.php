f975936bbf135f9e47edbea19583c549b2b084b8
kamissvitoria <kmorssantos@gmail.com>
2024-03-08 10:23:46 -0300

    exercicio 2

--- routes/api.php
+++ routes/api.php
@@ -8,3 +8,15 @@ Route::get('/nome', function(Request $request){
     $nome = $request->input("nome"); 
 
     return $nome; 
+
+});
+
+Route::get('dados', function(Request $request){ 
+
+    $Dado1 = $request->input("nome"); 
+
+    $Dado2 = $request->input("idade"); 
+
+    return 'Meu nome é ' . $Dado1 . ', tenho ' . $Dado2 . ' anos de idade'; 
+
+}); 
