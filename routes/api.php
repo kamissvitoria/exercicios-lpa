0e9bab0f05ef9f242a4319ff0538b9f1d6960863
kamissvitoria <kmorssantos@gmail.com>
2024-03-08 10:36:36 -0300

    Exercicio 4

--- routes/api.php
+++ routes/api.php
@@ -32,3 +32,17 @@ Route::get('dadoSSS', function(Request $request){
     return 'Meu nome é ' . $Dado1 . ', nasci no ano de ' . $Dado2 . ' ,na cidade de ' . $Dado3; 
 
 }); 
+
+Route::get('Subtração', function(Request $request){ 
+
+    $Primeiraentrada = $request->input("Primeiranota"); 
+
+    $Segundaentrada = $request->input("Segundanota"); 
+
+    $Terceiraentrada = $request->input("Terceiranota"); 
+
+    $Resultado = $Primeiraentrada - $Segundaentrada - $Terceiraentrada; 
+
+    return 'O resultado da subtração é ' . $Primeiraentrada . '-'. $Segundaentrada . '-' . $Terceiraentrada . '=' . $Resultado; 
+
+}); 
